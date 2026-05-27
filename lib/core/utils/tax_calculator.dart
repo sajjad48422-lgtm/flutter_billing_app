1e2bc7ba8311f2c6b5e01729a853216c04ac4221
sajjad48422-lgtm <sajjad48422@gmail.com>
2026-05-27 22:13:14 +0330

    Create tax_calculator.dart

---
+++ lib/core/utils/tax_calculator.dart
@@ -0,0 +1,20 @@
+class TaxCalculator {
+  /// نرخ مالیات بر ارزش افزوده ایران ۹٪
+  static const double vatRate = 0.09;
+
+  /// محاسبه مبلغ مالیات
+  static double calculateVat(double subtotal) {
+    return subtotal * vatRate;
+  }
+
+  /// مبلغ کل با مالیات
+  static double totalWithVat(double subtotal) {
+    return subtotal + calculateVat(subtotal);
+  }
+
+  /// درصد نمایشی
+  static String get vatPercent => '۹٪';
+
+  /// نام کامل
+  static String get vatLabel => 'مالیات بر ارزش افزوده (۹٪)';
+}
