bd11bf9278b9644a7a670744a488e69a690a9927
sajjad48422-lgtm <sajjad48422@gmail.com>
2026-05-31 10:14:22 +0330

    Update shop_model.dart

--- lib/features/shop/data/models/shop_model.dart
+++ lib/features/shop/data/models/shop_model.dart
@@ -23,6 +23,9 @@ class ShopModel extends Shop {
   @override
   @HiveField(5)
   final String footerText;
+  @override
+  @HiveField(6)
+  final String logoPath;
 
   const ShopModel({
     required this.name,
@@ -31,6 +34,7 @@ class ShopModel extends Shop {
     required this.phoneNumber,
     required this.upiId,
     required this.footerText,
+    this.logoPath = '',
   }) : super(
           name: name,
           addressLine1: addressLine1,
@@ -38,6 +42,7 @@ class ShopModel extends Shop {
           phoneNumber: phoneNumber,
           upiId: upiId,
           footerText: footerText,
+          logoPath: logoPath,
         );
 
   factory ShopModel.fromEntity(Shop shop) {
@@ -48,6 +53,7 @@ class ShopModel extends Shop {
       phoneNumber: shop.phoneNumber,
       upiId: shop.upiId,
       footerText: shop.footerText,
+      logoPath: shop.logoPath,
     );
   }
 
