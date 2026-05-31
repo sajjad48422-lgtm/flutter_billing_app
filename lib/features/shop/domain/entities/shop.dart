4eb3b3eddc6ab0c37a85fc446ccb85fc4462de87
sajjad48422-lgtm <sajjad48422@gmail.com>
2026-05-31 10:15:31 +0330

    Update shop.dart

--- lib/features/shop/domain/entities/shop.dart
+++ lib/features/shop/domain/entities/shop.dart
@@ -7,6 +7,7 @@ class Shop extends Equatable {
   final String phoneNumber;
   final String upiId;
   final String footerText;
+  final String logoPath;
 
   const Shop({
     this.name = '',
@@ -15,6 +16,7 @@ class Shop extends Equatable {
     this.phoneNumber = '',
     this.upiId = '',
     this.footerText = '',
+    this.logoPath = '',
   });
 
   Shop copyWith({
@@ -24,6 +26,7 @@ class Shop extends Equatable {
     String? phoneNumber,
     String? upiId,
     String? footerText,
+    String? logoPath,
   }) {
     return Shop(
       name: name ?? this.name,
@@ -32,10 +35,11 @@ class Shop extends Equatable {
       phoneNumber: phoneNumber ?? this.phoneNumber,
       upiId: upiId ?? this.upiId,
       footerText: footerText ?? this.footerText,
+      logoPath: logoPath ?? this.logoPath,
     );
   }
 
   @override
   List<Object?> get props =>
-      [name, addressLine1, addressLine2, phoneNumber, upiId, footerText];
+      [name, addressLine1, addressLine2, phoneNumber, upiId, footerText, logoPath];
 }
