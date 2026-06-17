e3a1b23c598a375f26580213dbadc157de7e415e
sajjad48422-lgtm <sajjad48422@gmail.com>
2026-06-18 02:18:11 +0330

    Add files via upload

---
+++ lib/features/billing/presentation/bloc/billing_state.dart
@@ -0,0 +1,73 @@
+part of 'billing_bloc.dart';
+
+class BillingState extends Equatable {
+  final List<CartItem> cartItems;
+  final String? error;
+  final bool isPrinting;
+  final bool printSuccess;
+  final bool isSendingSms;
+  final bool smsSuccess;
+  final Product? pendingWeightProduct;
+  final bool isSavingSale;
+  final bool saleSaved;
+
+  const BillingState({
+    this.cartItems = const [],
+    this.error,
+    this.isPrinting = false,
+    this.printSuccess = false,
+    this.isSendingSms = false,
+    this.smsSuccess = false,
+    this.pendingWeightProduct,
+    this.isSavingSale = false,
+    this.saleSaved = false,
+  });
+
+  double get subtotal =>
+      cartItems.fold(0, (sum, item) => sum + item.total);
+
+  double get vatAmount => subtotal * 0.09;
+
+  double get totalAmount => subtotal + vatAmount;
+
+  BillingState copyWith({
+    List<CartItem>? cartItems,
+    String? error,
+    bool clearError = false,
+    bool? isPrinting,
+    bool? printSuccess,
+    bool? isSendingSms,
+    bool? smsSuccess,
+    Product? pendingWeightProduct,
+    bool clearPendingProduct = false,
+    bool? isSavingSale,
+    bool? saleSaved,
+  }) {
+    return BillingState(
+      cartItems: cartItems ?? this.cartItems,
+      error: clearError ? null : (error ?? this.error),
+      isPrinting: isPrinting ?? this.isPrinting,
+      printSuccess: printSuccess ?? this.printSuccess,
+      isSendingSms: isSendingSms ?? this.isSendingSms,
+      smsSuccess: smsSuccess ?? this.smsSuccess,
+      pendingWeightProduct: clearPendingProduct
+          ? null
+          : (pendingWeightProduct ?? this.pendingWeightProduct),
+      isSavingSale: isSavingSale ?? this.isSavingSale,
+      saleSaved: saleSaved ?? this.saleSaved,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        cartItems,
+        error,
+        isPrinting,
+        printSuccess,
+        isSendingSms,
+        smsSuccess,
+        pendingWeightProduct,
+        isSavingSale,
+        saleSaved,
+      ];
+}
